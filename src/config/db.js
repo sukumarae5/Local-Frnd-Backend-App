a4d4bcb2b9f591481c414312ae68e0511251b95b
lokesh1561 <lokeshpommidhi@gmail.com>
2025-10-16 11:48:32 +0530

    iam update  backend code

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,30 @@
 const mysql=require("mysql2/promise")
-
-const connectionPool=mysql.createPool({
+require("dotenv").config("../.env")
+const fs=require("fs")
+const connection=mysql.createPool({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    name:process.env.DB_NAME,
+    ssl:{
+     ca:fs.readFileSync(process.env.CA) 
+    },
+    waitForConnections:true,
+    connectionLimit:0,
+    queueLimit:0
     
 })
+const loki = async()=>{
+    try {
+        const con= await connection.getConnection()
+        console.log(con)
+        
+    } catch (error)
+    {
+        console.log(error)
+        
+    }
+}
+
+loki()
+module.exports=connection
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const db=require("../src/config/db")
 const app=express();
 const port=500
 app.use(express.json());
 app.listen(port,()=>{
     console.log("serever is running")
     console.log("i am clone successfuly")
-    console.log("i am vijaya clone successfully")
+    console.log("i am lokesh  clone successfully ")
 })
